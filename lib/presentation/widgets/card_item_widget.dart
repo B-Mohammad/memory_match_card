0f7dfcd8254853fbcb3b850932dadd19cf101cf0
Mohammadb <mohammad.mb.bashirinia@gmail.com>
2024-07-06 09:47:04 +0330

    edit card item

--- lib/presentation/widgets/card_item_widget.dart
+++ lib/presentation/widgets/card_item_widget.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:memory_match_card/models/card_item_model.dart';
 
 class CardItemWidget extends StatefulWidget {
-  const CardItemWidget({super.key});
+  final int num;
+  const CardItemWidget({super.key, required this.num});
 
   @override
   State<CardItemWidget> createState() => _CardItemWidgetState();
 }
 
 class _CardItemWidgetState extends State<CardItemWidget> {
+  double _width = 100;
+  final double _height = 150;
+  Mode mode = Mode.hide;
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(seconds: 2),
-      child: SvgPicture.asset("images/card_asset.svg"),
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _width = 0;
+        });
+      },
+      child: AnimatedContainer(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        decoration: BoxDecoration(
+          color: Colors.grey[50],
+          borderRadius: BorderRadius.circular(4),
+        ),
+        duration: const Duration(milliseconds: 80),
+        width: _width,
+        height: _height,
+        curve: Curves.fastOutSlowIn,
+        onEnd: () {
+          if (_width == 0 && mode == Mode.hide) {
+            setState(() {
+              _width = 100;
+              mode = Mode.visible;
+            });
+          } else if (_width == 0 && mode == Mode.visible) {
+            setState(() {
+              _width = 100;
+              mode = Mode.hide;
+            });
+          }
+        },
+        child: mode == Mode.hide
+            ? SvgPicture.asset(
+                fit: BoxFit.fitHeight,
+                "images/card_asset.svg",
+              )
+            : Text(
+                "widget.num.toString()",
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+      ),
     );
   }
 }
