c3a3274b36095515cef915eac0e926612d374259
Mohammadb <mohammad.mb.bashirinia@gmail.com>
2024-07-28 12:27:09 +0330

    fix name

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Memory Match Card Home Page'),
+      home: const MyHomePage(title: 'Memory Match Card'),
     );
   }
 }
@@ -37,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
